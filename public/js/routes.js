1dcefe2e1638beafd959b1ffc4e15f30e94582db
gbChoi <mosfet1kg@kaist.ac.kr>
2014-09-29 19:31:05 +0900

    MCU updated

--- public/js/routes.js
+++ public/js/routes.js
@@ -22,7 +22,7 @@ define([
 		            }
 		        })
 		        .state('public.404', {
-		            url: '/404/',
+		            url2cduw: '/404/',
 		            templateUrl: '404'
 		        })
 		        .state('public.lapp', {
